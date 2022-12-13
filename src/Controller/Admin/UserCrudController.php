d7f8da5cddcbf9e19aafd0a182ae550c5ff70eff
fcomarcet1 <fcomarcet1@gmail.com>
2022-12-13 09:49:40 +0100

    ChoiceField for Roles

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,9 +6,11 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -22,15 +24,23 @@ class UserCrudController extends AbstractCrudController
     {
 
         yield EmailField::new('email');
-        yield TextField::new('firstName')->onlyOnForms();;
+        yield TextField::new('firstName')->onlyOnForms();
         yield TextField::new('lastName')->onlyOnForms();
         //yield TextField::new('fullName')->onlyOnIndex();
-        yield TextField::new('fullName')->hideOnForm();;
-        yield ArrayField::new('roles');
+        yield TextField::new('fullName')->hideOnForm();
+
+        $roles = ['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER'];
+        yield ChoiceField::new('roles')
+            ->setChoices(array_combine($roles, $roles))
+            ->allowMultipleChoices()
+            ->setHelp('Available roles: ROLE_SUPER_ADMIN, ROLE_ADMIN, ROLE_MODERATOR, ROLE_USER')
+            ->renderExpanded()
+            ->renderAsBadges();
+
         yield BooleanField::new('enabled');
             //->renderAsSwitch(false);
-        yield DateField::new('createdAt')->hideOnForm();;
-        yield DateField::new('updatedAt')->hideOnForm();;
+        yield DateField::new('createdAt')->hideOnForm();
+        yield DateField::new('updatedAt')->hideOnForm();
         /*return [
             IdField::new('id'),
             TextField::new('title'),
