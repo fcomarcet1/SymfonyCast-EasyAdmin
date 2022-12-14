b5f81e779ad1621a64cb75409bbfc5b832c9ef50
fcomarcet1 <fcomarcet1@gmail.com>
2022-12-14 15:33:39 +0100

    Auto-complete Association Field & Controlling the Query

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
@@ -26,7 +27,22 @@ class QuestionCrudController extends AbstractCrudController
             ->hideOnIndex();
         yield Field::new('votes', 'Total Votes')
             ->setTextAlign('center');
-        yield AssociationField::new('askedBy');
+        yield AssociationField::new('askedBy')
+            ->formatValue(static function ($value, Question $question): ?string {
+                if (!$user = $question->getAskedBy()) {
+                    return null;
+                }
+                return sprintf('%s&nbsp;(%s)', $user->getEmail(), $user->getQuestions()->count());
+            })
+            // Let's restrict this to only enabled users
+            ->setQueryBuilder(function (QueryBuilder $qb) {
+                $qb->andWhere('entity.enabled = :enabled')
+                    ->setParameter('enabled', true);
+            })
+            // in this case, every user in the database, is loaded onto the page in the background to build the
+            // select. This means that if you have even a hundred users in your database,
+            // this page is going to start slowing down, and eventually, explode. we can use ->autocomplete()
+            ->autocomplete();
         yield Field::new('createdAt')
             ->hideOnForm();
         yield Field::new('updatedAt')
