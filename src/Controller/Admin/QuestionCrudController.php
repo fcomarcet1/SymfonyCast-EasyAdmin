a8ab050b6d8252fc6d6e30278a1a476e92187f5c
fcomarcet1 <fcomarcet1@gmail.com>
2022-12-15 08:10:31 +0100

    Creating a Custom Field

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\EasyAdmin\VotesField;
 use App\Entity\Question;
 use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -9,6 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class QuestionCrudController extends AbstractCrudController
@@ -34,7 +36,10 @@ class QuestionCrudController extends AbstractCrudController
         yield AssociationField::new('topic');
         yield TextareaField::new('question')
             ->hideOnIndex();
-        yield Field::new('votes', 'Total Votes')
+       /* yield IntegerField::new('votes', 'Total Votes')
+            ->setTextAlign('center')
+            ->setTemplatePath('admin/field/votes.html.twig');*/
+        yield VotesField::new('votes', 'Total Votes')
             ->setTextAlign('center');
         yield AssociationField::new('askedBy')
             ->formatValue(static function ($value, Question $question): ?string {
